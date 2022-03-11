8fa3b2b984b4704c5c5702a047214120b6aaa477
lucascaiafa00 <100371782+lucascaiafa00@users.noreply.github.com>
2022-03-11 15:00:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ let artworks = document.querySelectorAll(".artwork")
 let previousButton = document.getElementById("previous")
 let nextButton = document.getElementById("next")
 let fullImage = document.getElementById("full-image")
+var imageNumber = 0
+var infoFullImage = false
+var slideShow
+
 
 function openInfo(title, image, text, link){
     background.classList.add("open")
@@ -18,10 +22,32 @@ function openInfo(title, image, text, link){
     info.children[3].href = link
 }
 
-info.children[1].onclick = ()=>{
+function nextImage(){
+    fullImage.src = artworks[imageNumber].src
     fullImage.classList.add("open")
+
+    imageNumber++
+
+    if(imageNumber >= artworks.length){
+        imageNumber = 0
+    }
+    setTimeout(()=>{fullImage.classList.remove("open")},7000)
+}
+
+function startSlideShow() {
+    background.classList.add("open")
+    nextImage()
+    slideShow = setInterval(nextImage,8000)
+    
+}
+
+//startSlideShow()
+
+info.children[1].onclick = ()=>{
     fullImage.src = info.children[1].src
+    fullImage.classList.add("open")
     info.classList.remove("open")
+    infoFullImage = true
 }
 
 for (let index = 0; index < artworks.length; index++) {
@@ -32,16 +58,17 @@ for (let index = 0; index < artworks.length; index++) {
     })
 }
 
-
 background.addEventListener('click', ()=>{
-    if (fullImage.classList.contains("open")) {
-        fullImage.classList.remove("open")
+    if (infoFullImage === true) {
         info.classList.add("open")
+        infoFullImage = false
     }else{
         background.classList.remove("open")
         info.classList.remove("open")
 
     }
-
+    clearInterval(slideShow)
+    fullImage.classList.remove("open")
+    imageNumber = 0
     document.body.style.overflow = "auto"
 })
